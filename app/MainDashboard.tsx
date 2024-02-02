b59745bbc50056a11efb9ba27508fe8f78371438
Brandon Herman <brandon.herman@dialexa.com>
2024-02-02 16:23:40 -0600

    Moved picker card to maindashboard component, added json server

---
+++ app/MainDashboard.tsx
@@ -0,0 +1,22 @@
+import PickerCard from "./components/pickerCard/PickerCard";
+
+async function getGames() {
+    const res = await fetch('http://localhost:4000/games');
+    return res.json()
+}
+
+export default async function MainDashboard() {
+    const games = await getGames();
+    return (
+      <div className="z-10 flex-col max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        {games.map((game: any, i: number) => (
+          <div key={i} className=' w-full pb-10'>
+          <PickerCard gameInfo={game} key={i}/>
+          </div>
+         ))}
+         {games.length === 0 && (
+            <p className="text-center">There are no upcoming games.</p>
+         )}
+      </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
-import PickerCard from "./components/pickerCard/PickerCard";
-import gameInfo from '../public/dummyData.json'
+import MainDashboard from "./MainDashboard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 flex-col max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        {gameInfo.map((game, i) => (
-          <div key={i} className=' w-full pb-10'>
-          <PickerCard gameInfo={game} key={i}/>
-          </div>
-         ))}
-      </div>
-
+      <MainDashboard/>
     </main>
   );
 }
